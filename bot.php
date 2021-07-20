5317f78537dd6213c16db73126357b498367189c
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-07-20 16:35:54 -0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,30 +3,24 @@
 require('parser.php');
 
 define('BOT_TOKEN', '1946698638:AAFYoGL1j85v_thS7YwIHJ3Kspzal_qeepI');
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+define('API_URL', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');
 
-function processMessage($message) {
+function processMessage($message)
+{
   // processa a mensagem recebida
   $message_id = $message['message_id'];
   $chat_id = $message['chat']['id'];
   if (isset($message['text'])) {
-    
-    $text = $message['text'];//texto recebido na mensagem
+
+    $text = $message['text']; //texto recebido na mensagem
 
     if (strpos($text, "/start") === 0) {
-		//envia a mensagem ao usuário
-      sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Olá, '. $message['from']['first_name'].
-		'! Eu sou um bot que informa o resultado do último sorteio da Mega Sena. Será que você ganhou dessa vez? Para começar, escolha qual loteria você deseja ver o resultado', 'reply_markup' => array(
-        'keyboard' => array(array('Mega-Sena', 'Quina'),array('Lotofácil','Lotomania')),
-        'one_time_keyboard' => true)));
-    } else if ($text === "Mega-Sena") {
-      sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('megasena', $text)));
-    } else if ($text === "Quina") {
-      sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('quina', $text)));
-    } else if ($text === "Lotomania") {
-      sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('lotomania', $text)));
-    } else if ($text === "Lotofácil") {
-      sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('lotofacil', $text)));
+      //envia a mensagem ao usuário
+      sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Olá, ' . $message['from']['first_name'] .
+        '! Eu sou um bot que informa o resultado do último sorteio da Mega Sena. Será que você ganhou dessa vez? Para começar, escolha qual loteria você deseja ver o resultado', 'reply_markup' => array(
+        'keyboard' => array(array('Mega-Sena', 'Quina'), array('Lotofácil', 'Lotomania')),
+        'one_time_keyboard' => true
+      )));
     } else {
       sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Desculpe, mas não entendi essa mensagem. :('));
     }
@@ -35,18 +29,19 @@ function processMessage($message) {
   }
 }
 
-function sendMessage($method, $parameters) {
+function sendMessage($method, $parameters)
+{
   $options = array(
-  'http' => array(
-    'method'  => 'POST',
-    'content' => json_encode($parameters),
-    'header'=>  "Content-Type: application/json\r\n" .
-                "Accept: application/json\r\n"
+    'http' => array(
+      'method'  => 'POST',
+      'content' => json_encode($parameters),
+      'header' =>  "Content-Type: application/json\r\n" .
+        "Accept: application/json\r\n"
     )
-);
+  );
 
-$context  = stream_context_create( $options );
-file_get_contents(API_URL.$method, false, $context );
+  $context  = stream_context_create($options);
+  file_get_contents(API_URL . $method, false, $context);
 }
 
 /*Com o webhook setado, não precisamos mais obter as mensagens através do método getUpdates.Em vez disso, 
@@ -60,5 +55,3 @@ $update = json_decode($update_response, true);
 if (isset($update["message"])) {
   processMessage($update["message"]);
 }
-
-?>
